95af4a3693eb499e78e083988363f010a0cbab4b
CafeCodr <114000708+cafecodr@users.noreply.github.com>
2022-09-22 13:37:12 -0400

    Create script.py with global variables for constants

---
+++ script.py
@@ -0,0 +1,12 @@
+import random
+import time
+
+rock = 1
+paper = 2
+scissors = 3
+
+names = {rock: "Rock", paper: "Paper", scissors: "Scissors"}
+rules = {rock: scissors, paper: rock, scissors: paper}
+
+player_score = 0
+computer_score = 0
